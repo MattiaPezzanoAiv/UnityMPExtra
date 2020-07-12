6e0b62f9878b662bb48913f1ef60fe17a4da6c04
MattiaPezzanoAiv <mattiapezzano7@gmail.com>
2020-07-12 19:25:16 +0100

    added io utils file to write with 1 line of code

---
+++ Assets/MP/IOUtils/IOExample.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IOExample : MonoBehaviour
+{
+    private void Start()
+    {
+        IOUtils.SafeWriteFile(Application.persistentDataPath + "/TestFolder/RandomFile.txt", "This is a test file");
+        IOUtils.SafeWriteFileAsync(Application.persistentDataPath + "/TestFolder/RandomFile2.txt", "This is a test file", () => Debug.Log("Completed"));
+    }
+}
---
+++ Assets/MP/IOUtils/IOUtils.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+
+public static class IOUtils
+{
+    private static string PreprocessPath(string path)
+    {
+        string result = path;
+        if (!Path.HasExtension(path))
+        {
+            result += ".txt";
+        }
+
+        // if file doesn't exist we create the whole directory path then use a stream writer to be safe
+        if (!File.Exists(path))
+        {
+            var dir = Path.GetDirectoryName(path);
+            Directory.CreateDirectory(dir);
+        }
+
+        return result;
+    }
+
+    public static void SafeWriteFile(string path, string content)
+    {
+        var filePath = PreprocessPath(path);
+
+        using (var sw = new StreamWriter(filePath))
+        {
+            sw.Write(content);
+        }
+    }
+
+    public static void SafeWriteFileAsync(string path, string content, System.Action onComplete)
+    {
+        var filePath = PreprocessPath(path);
+        MP.Coroutines.GlobalCoroutineRunner.Instance.StartCoroutine(WriteFileAsyncCoroutine(filePath, content, onComplete));
+    }
+
+    private static System.Collections.IEnumerator WriteFileAsyncCoroutine(string path, string content, System.Action onComplete)
+    {
+        using (var sw = new StreamWriter(path))
+        {
+            var task = sw.WriteAsync(content);
+
+            while(!task.IsCompleted)
+            {
+                yield return null;
+            }
+
+            if(task.IsFaulted)
+            {
+                throw new UnityEngine.UnityException(task.Exception.Message);
+            }
+
+            onComplete?.Invoke();
+        }
+    }
+}
